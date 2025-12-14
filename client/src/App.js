7d04705532b8a765974753118ab7f89ab552bad2
iamatultiwwari <atult2386@gmail.com>
2025-12-14 19:35:58 +0530

    added some feature in ui

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import Home from './components/screens/Home';
 import Navbar from './components/screens/Navbar';
 import SignIn from './components/screens/SignIn';
 import SignUp from './components/screens/SignUp';
+import Footer from './components/screens/Footer';   // ✅ ADD THIS
 import {
   BrowserRouter,
   Routes,
   Route
-} from "react-router-dom"
+} from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { LoginContext } from './context/LoginState';
@@ -17,40 +18,50 @@ import CreatePost from './components/screens/CreatePost';
 import Profile from './components/screens/Profile';
 import MyFollowingPost from './components/screens/MyFollowingPost';
 import UserProfile from './components/screens/UserProfile';
+
 let base_uri = process.env.REACT_APP_BASE_URL;
 
 function App() {
 
-  const {modalOpen} = useContext(LoginContext)
-  const {loggedUser, setLoggedUser} = useContext(LoginContext)
+  const { modalOpen } = useContext(LoginContext);
+  const { loggedUser, setLoggedUser } = useContext(LoginContext);
 
-  useEffect(()=>{
-    if(localStorage.getItem("user")){
-      fetch(`${base_uri}/user/${JSON.parse(localStorage.getItem("user"))._id}`, {
-        headers: {
-          "Authorization": "Bearer "+localStorage.getItem("jwt")
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      fetch(
+        `${base_uri}/user/${JSON.parse(localStorage.getItem("user"))._id}`,
+        {
+          headers: {
+            "Authorization": "Bearer " + localStorage.getItem("jwt")
+          }
         }
-      }).then(res => res.json())
-      .then((result)=>{
-        setLoggedUser(result.user)
-      })
+      )
+        .then(res => res.json())
+        .then((result) => {
+          setLoggedUser(result.user);
+        });
     }
-  }, [loggedUser ? loggedUser.Photo : [] ])
+  }, [loggedUser ? loggedUser.Photo : []]);
 
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
+
       <Routes>
-        <Route path='/signup' element={<SignUp/>} />
-        <Route path='/signin' element={<SignIn/>} />
-        <Route path='/' element={<Home/>} />
-        <Route path='/createPost' element={<CreatePost/>} />
-        <Route exact path='/profile' element={<Profile/>} />
-        <Route path='/followingpost' element={<MyFollowingPost/>} />
-        <Route path='/profile/:userid' element={<UserProfile/>} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/createPost' element={<CreatePost />} />
+        <Route exact path='/profile' element={<Profile />} />
+        <Route path='/followingpost' element={<MyFollowingPost />} />
+        <Route path='/profile/:userid' element={<UserProfile />} />
       </Routes>
+
+      {/* ✅ FOOTER ADDED HERE */}
+      <Footer />
+
       <ToastContainer theme='dark' />
-      {modalOpen && <Modal/>}
+      {modalOpen && <Modal />}
     </BrowserRouter>
   );
 }
---
+++ client/src/components/screens/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="insta-footer">
+      <div className="footer-links">
+        <a href="#">Meta</a>
+        <a href="#">About</a>
+        <a href="#">Blog</a>
+        <a href="#">Jobs</a>
+        <a href="#">Help</a>
+        <a href="#">API</a>
+        <a href="#">Privacy</a>
+        <a href="#">Terms</a>
+        <a href="#">Locations</a>
+        <a href="#">Instagram Lite</a>
+        <a href="#">Threads</a>
+        <a href="#">Contact Uploading & Non-Users</a>
+        <a href="#">Meta Verified</a>
+      </div>
+
+      <div className="footer-bottom">
+        <span>English</span>
+        <span>© 2025 Instagram from Meta</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
